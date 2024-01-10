9907a4641d6e37ba9016f04629710c0a2b823dc3
ai-application <156155368+ai-application@users.noreply.github.com>
2024-01-10 14:07:05 +0800

    Add files via upload

---
+++ classification/models/__init__.py
@@ -0,0 +1,4 @@
+from .cmbFormer import CmbFormer_S, CmbFormer_B
+from .resnet3D import resNet3D_18, resNet3D_50, resNet3D_101
+from .uniformer import uniformer_xxs, uniformer_xs
+from .uniformerv2 import UniFormerv2_b16, UniFormerv2_114
